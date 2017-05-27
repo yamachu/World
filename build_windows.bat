ff87f36f18f03b3d9d4e7edcb5d2ee2ca660a0a5
Yusuke Yamada <yamachu.dev@gmail.com>
2017-05-27 17:47:33 +0900

    Update build bat

--- build_windows.bat
+++ build_windows.bat
@@ -1,2 +1,9 @@
+cd /d %~dp0
+if %ARCH% == x86 (
+    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+) else (
+    call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
+    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
+)
 cl.exe /c src\*.cpp tools\*.cpp
-link.exe /dll /def:world.def *obj
+link.exe /dll /def:world.def *obj /OUT:build\%ARCH%_world.dll
