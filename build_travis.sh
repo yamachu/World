431d03053e785b86691f7a52890dcec31c2a827d
Yusuke Yamada <yamachu.dev@gmail.com>
2017-05-29 10:33:47 +0900

    Add Android build

--- build_travis.sh
+++ build_travis.sh
@@ -3,5 +3,6 @@
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   make mac_shared
 else
-  make linux_shared
+  make linux_shared CXX=$CXX CXXFLAGS="${CXXFLAGS}" LDFLAGS="$LDFLAGS"
+  mv build/libworld.so build/${_ARCHNAME_}libworld.so
 fi
