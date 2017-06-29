5abe30df86710991c45b77ad4295bfffb39d811f
Yusuke Yamada <yamachu.dev@gmail.com>
2017-06-29 17:20:39 +0900

    Add get tag and hash interface

---
+++ utils/version.h
@@ -0,0 +1,18 @@
+#ifndef WORLD_UTILS_VERSION_H_
+#define WORLD_UTILS_VERSION_H_
+
+#ifndef WORLD_LIBRARY_VERSION
+#define WORLD_LIBRARY_VERSION "v0.0.0_0000000"
+#endif // WORLD_LIBRARY_VERSION
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const char *getWorldLibraryInfo(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // WORLD_UTILS_VERSION_H_
